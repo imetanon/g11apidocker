f384e827f7d79a606c1b0b66e0c6c12083e00df8
Metanon J <imetanon@gmail.com>
2020-10-24 18:46:09 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -56,6 +56,7 @@ DESCRIPTION_PREDICT_LABELS = ['Action', 'Adult', 'Adventure', 'Animation', 'Biog
                               'Horror', 'Music', 'Musical', 'Mystery', 'Romance', 'Sci-Fi',
                               'Short', 'Sport', 'Thriller', 'War', 'Western']
 
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -65,9 +66,12 @@ api = Api(app)
 poster_model = tf.keras.models.load_model(
     "model_20200829.h5", compile=False, custom_objects={'KerasLayer': hub.KerasLayer})
 
+place_model = tf.keras.models.load_model("model_ResNetV2.h5")
+
 # NLP Model
 description_model = pickle.load(open('model_description_20200831.pkl', 'rb'))
 tf1 = pickle.load(open("tfidf1.pkl", 'rb'))
+
 
 @app.route("/")
 def hello():
@@ -104,6 +108,47 @@ def poster_predict(image_path, isUrl=False):
     response = {}
     response['predict_genres'] = [
         f'{POSTER_PREDICT_LABELS[p]}: {predict_value.tolist()[0][p]:.2f}' for p in prediction]
+
+    return response
+
+
+def place_predict(image_path, isUrl=False):
+    if isUrl:
+        img_path = tf.keras.utils.get_file(fname=next(
+            tempfile._get_candidate_names()), origin=image_path)
+    else:
+        img_path = image_path
+
+    img = keras.preprocessing.image.load_img(
+        img_path, color_mode='rgb', target_size=(IMG_SIZE, IMG_SIZE)
+    )
+
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = img_array/255
+    img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+    # Generate prediction
+    predict = place_model.predict(img_array)
+    result = np.argmax(predict, axis=1)
+
+    dict_mapping = {
+        0: 'คาเฟ่ / ร้านกาแฟ / ร้านขนม / เบเกอรี่',
+        1: 'ผับ บาร์ สถานที่ท่องเที่ยวยามค่ำคืน',
+        2: 'ร้านอาหาร',
+        3: 'เน้นกิจกรรม เช่น สวนสนุก สวนน้ำ ฟาร์ม พิพิธภัณฑ์',
+        4: 'เน้นช๊อปปิ้ง เช่น ตลาด / ตลาดกลางคืน / สตรีทฟูดส์ / ห้างสรรพสินค้า',
+        5: 'เน้นทะเล / ชายหาด / กิจกรรมทางน้ำ',
+        6: 'เน้นประวัติศาสตร์ / วัฒนธรรม',
+        7: 'เน้นป่า / ภูเขา / จุดชมวิว',
+        8: 'เน้นสักการะสิ่งศักดิ์สิทธิ์ เช่น วัด / ศาลเจ้า / โบสถ์'
+    }
+
+    category_list = np.vectorize(dict_mapping.get)(result).tolist()
+
+    os.remove(img_path)
+
+    response = {}
+    response['category'] = category_list
 
     return response
 
@@ -204,6 +249,22 @@ class TextGenre(Resource):
             'predict_genres']
         return response
 
+class ImagePlace(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('imgurl', required=True,
+                    help="Please Specify Image Url !!!")
+        super(ImageGenre, self).__init__()
+
+    def get(self):
+        args = self.reqparse.parse_args()
+        image_url = args['imgurl']
+        response = {}
+        response['source'] = image_url
+        response['predict_genres'] = dict(poster_predict(
+            image_url, isUrl=True))['predict_genres']
+        return response
+
 
 ##
 # Actually setup the Api resource routing here
@@ -211,3 +272,4 @@ class TextGenre(Resource):
 api.add_resource(Imdb, '/imdb')
 api.add_resource(ImageGenre, '/genre/image')
 api.add_resource(TextGenre, '/genre/text')
+api.add_resource(ImagePlace, '/place/image')
